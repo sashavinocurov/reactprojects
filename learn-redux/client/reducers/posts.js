c357a3872dc6e85c096fe6fd6cc0fd75a1b10667
Alexandr Vinocurov <vinocurovxandr@gmail.com>
2020-06-01 17:20:21 -0700

    learn-redux

---
+++ learn-redux/client/reducers/posts.js
@@ -0,0 +1,24 @@
+// a reducer takes in two things 
+
+// 1. the action (info about what happened)
+// 2. A copy of the current state
+
+// action, store     ..   okay let me see    ... store
+
+function posts (state = [], action) {
+    switch(action.type) {
+        case 'INCREMENT_LIKES' :
+            const i = action.index;
+        // return the updated state 
+        return [
+            ...state.slice(0,i), // before the one we are updating
+            {...state[i], likes: state[i].likes + 1},
+            ...state.slice(i + 1),  // after the one we are updating
+        ];
+        default:
+            return state;
+    }
+}
+
+export default posts;
+
---
+++ learn-redux/client/reduxstagram.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+import { render } from 'react-dom';
+
+//import css
+import css from './styles/style.styl';
+
+//import Components 
+import App from './components/App';
+import Single from './components/Single';
+import PhotoGrid from './components/PhotoGrid';
+
+//import react router deps
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+import { Provider } from 'react-redux';
+import store, { history } from './store';
+
+import Raven from 'raven-js';
+import { sentry_url } from './data/config';
+
+Raven.config(sentry_url).install();
+
+const router = (
+    <Provider store={store}>
+        <Router history={history}>
+            <Route path="/" component={App}>
+                <IndexRoute component={PhotoGrid}></IndexRoute>
+                <Route path="/view/:postId" component={Single}></Route>
+            </Route>
+        </Router>
+    </Provider>
+)
+
+render(router, document.getElementById('root'));
+
+
